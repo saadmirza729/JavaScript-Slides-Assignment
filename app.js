9f1014498a8d4e7abf7848e7c62793887546ba94
saadmirza729 <saadmirza729@gmail.com>
2025-11-09 13:16:40 +0500

    Add new files of JavaScript

---
+++ app.js
@@ -0,0 +1,174 @@
+//  1 to 10 
+// slide 2 ----- Helper Functions
+
+// document.writeln('Hello World');
+// alert('Hello Everyone')
+// console.log('Hello world')
+
+
+// slide 3 ----- Statements and comments
+
+// This is comment
+
+// I am Saad Mirza 
+// Student of SMIT
+
+// Here is console
+
+
+// console.log('Good Morning')
+// console.log('Welcome to my website')
+
+
+// Slide 4, 5 and 6  ----- Variables
+
+// How can we create a variable?
+
+// var myName = "Saad Mirza";
+// console.log(myName);
+
+
+// Slide 7 ----- Variables ways
+
+// var , let , const
+
+// var -- we can re-assign
+// var grade = "A+";
+
+// // let -- we can re-assign
+// var grade2 = "B+";
+
+
+// // const -- we cannot re-assign value
+// const grade3 = "C+"
+
+
+// slide 8 --------------- Challenge ---------------
+
+// let firstName;
+// let lastName;
+
+// firstName = "Saad";
+// lastName = "Mirza";
+
+// let address = "main street"
+// address = "first street"
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(address)
+
+
+// Slide 9 ----- Quotation "" or ''
+
+// const myName = "Saad Mirza";
+// console.log(myName)
+
+
+// Slide 10 ----- String Concatenation "" + ""
+
+
+// let webName = "google"
+
+// let url = 'https://www.' + webName + '.com';
+// console.log(url)
+
+
+//  11 to 20 
+// slide 11 --------------- Challenge Concatenation ---------------
+
+// 1) value1 + value2
+// 2) `${value1} ${value2}`
+
+
+// let street;
+// let country;
+
+// street = "Bufferzone Karachi";
+// country = "Pakistan"
+// let fullMailingAddress = `${street} ${country}`
+// console.log(fullMailingAddress)
+ 
+
+// slide 15 --------------- Challenge Numbers ---------------
+
+
+// let score1 = 70;
+// let score2 = 42;
+// let score3 = 55;
+
+// let totalScore = score1 + score2 + score3;
+// let averageScore = totalScore / 3;
+
+// console.log(totalScore)
+// console.log(averageScore)
+
+
+// let plates = 20;
+// let people = 7;
+// let remainingPlates = plates - people;
+// remainingPlates += 1;
+// let message = "There are " + remainingPlates + " plates available"
+// console.log(message)
+
+
+// slide 20 --------------- Challenge array ---------------
+
+
+// let fruits = ["mango" , "banana" , "orange" , "apple" ];
+
+// let numLast = fruits.length -1;
+
+// fruits[numLast] = Math.floor(Math.random() * 100)
+
+// let firstFriut = fruits[0];
+// console.log(fruits)
+
+// fruits[numLast] = "peach"
+
+// console.log(fruits)
+
+//  21 to 30 
+// slide 25 --------------- Challenge Function ---------------
+
+
+// function calculate (subTotal , Tax) {
+//     return subTotal + Tax;
+
+// }
+
+// let order1 = calculate(200 , 100)
+// let order2 = calculate(50 , 80)
+// let order3 = calculate(400 , 80)
+
+// console.log(order1)
+// console.log(order2)
+// console.log(order3)
+
+
+// slide 27 --------------- Challenge Object ---------------
+
+
+// let car = {
+//     make: "Honda",
+//     model: "Civic",
+//     year: 2024,
+//     colors: ["White" , "Black"],
+//     hybrid: true,
+//     drive: function (value1) {
+//         return value1
+//     },
+//     stop: function (value2) {
+//         return value2
+//     }
+// }
+
+
+// console.log(car.make)
+// console.log(car.colors[0])
+// console.log(car.drive("Car is moving"))
+// console.log(car.stop("Car has stoped"))
+
+
+//  31 to 40 
+// slide 35 --------------- Challenge conditional ---------------
